c631754f41797309d43d8588fc251457a8c5a3ed
Léo Chely <leo.chely@live.fr>
2022-03-28 19:33:36 -0600

    Fix

--- bot.py
+++ bot.py
@@ -148,8 +148,7 @@ class LeixBot(commands.Bot):
 
         if event.reward.title == "Giveaway":
             logging.info(f'{event.user.name} entered the giveaway!')
-        self.giveaway
-.add(event.user.name)
+            self.giveaway.add(event.user.name)
 
     ## ROUTINES ##
     @routines.routine(minutes=30.0, wait_first=False)
