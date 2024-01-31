04f08b1ec3e574fcfdfcb245e7d0e239a1ff8f79
Léo Chely <leo.chely@live.fr>
2024-01-30 23:30:38 -0700

    Adding AI commands with !chat

---
+++ cogs/ai.py
@@ -0,0 +1,48 @@
+import logging
+import os
+
+import aiohttp
+
+from twitchio.ext import commands
+
+LLM_API_URL = os.environ.get('LLM_API_URL')
+
+class AI(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.chat_history = {}
+
+    @commands.command(name="chat")
+    async def chat(self, ctx: commands.Context, *prompt):
+        """Chat avec l'IA de LeixBot. Ex: !chat Wsh t ki?"""
+        user = ctx.author.name
+        prompt = ' '.join(prompt)
+        self.chat_history.setdefault(user,[]).append(
+            {
+            "role": "user",
+            "content":prompt
+            },
+        )
+        logging.info(self.chat_history[user])
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                f"{LLM_API_URL}/api/chat", 
+                json={
+                    "messages": self.chat_history[user],
+                    "model": "leixbot",
+                    "stream":False,
+                    "use_mlock":True}) as resp:
+                data = await resp.json()
+                logging.info(data['message']['content'])
+                response = data['message']['content']
+                await ctx.send(response)
+                self.chat_history[user].append(
+                    {
+                    "role": "assistant",
+                    "content":response
+                    },
+                )
+
+
+def prepare(bot: commands.Bot):
+    bot.add_cog(AI(bot))
