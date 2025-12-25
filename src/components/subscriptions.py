aa8b773ad8dcd15e1e9a9607971fe26c4d478955
Léo Chely <leo.chely@live.fr>
2025-12-25 15:12:27 -0700

    Enabling events in component

--- src/components/subscriptions.py
+++ src/components/subscriptions.py
@@ -9,6 +9,7 @@ class Subscriptions(commands.Component):
     def __init__(self, bot: commands.AutoBot):
         self.bot = bot
 
+    @commands.Component.listener()
     async def event_follow(self, message: twitchio.ChannelFollow) -> None:
         channel = message.broadcaster.name
         follower = message.user.name
@@ -17,7 +18,8 @@ class Subscriptions(commands.Component):
             sender=self.bot_id,
             message=f"Merci pour le follow {follower}! PogChamp",
         )
-    
+
+    @commands.Component.listener()  
     async def event_subscription(self, message: twitchio.ChannelSubscribe) -> None:
         channel = message.broadcaster.name
         user = message.user.name
@@ -27,6 +29,7 @@ class Subscriptions(commands.Component):
             message=f"{user} rejoint la legion au tier {message.tier}! PogChamp",
         )
 
+    @commands.Component.listener()
     async def event_subscription_message(self, message: twitchio.ChannelSubscriptionMessage) -> None:
         channel = message.broadcaster.name
         user = message.user.name
@@ -37,6 +40,7 @@ class Subscriptions(commands.Component):
             message=f"{user} est dans la legion depuis {message.cumulative_months}! PogChamp",
         )
 
+    @commands.Component.listener()
     async def event_stream_online(self, message: twitchio.StreamOnline) -> None:
         LOGGER.info(f"{message.broadcaster.name} is live!")
         await message.broadcaster.send_message(
@@ -44,6 +48,7 @@ class Subscriptions(commands.Component):
             message=f"Coucou {message.broadcaster.display_name}, votre fidele LeixBot est pret a vous servir pour votre stream! PogChamp",
         )
 
+    @commands.Component.listener()
     async def event_stream_offline(self, message: twitchio.StreamOffline) -> None:
         LOGGER.info(f"{message.broadcaster.name} is offline now.")
         await message.broadcaster.send_message(
@@ -51,6 +56,7 @@ class Subscriptions(commands.Component):
             message=f"Au revoir {message.broadcaster.display_name}! A la prochaine fois! DinoDance",
         )
 
+    @commands.Component.listener()
     async def event_raid(self, message: twitchio.ChannelRaid) -> None:
         LOGGER.info(f"{message.from_broadcaster} is raiding {message.to_broadcaster} with {message.viewer_count} viewers!")
         await message.to_broadcaster.send_message(
@@ -58,6 +64,7 @@ class Subscriptions(commands.Component):
             message=f"Il faut se defendre SwiftRage Nous sommes raid par {message.from_broadcaster.display_name} avec ses {message.viewer_count} margoulins!",
         )
 
+    @commands.Component.listener()
     async def event_ad_break(self, message: twitchio.ChannelAdBreakBegin) -> None:
         LOGGER.info(f"Ad break started on {message.broadcaster.name} for {message.duration} seconds!")
         await message.broadcaster.send_message(
@@ -65,6 +72,7 @@ class Subscriptions(commands.Component):
             message=f"Pub en cours mais ne vous inquietez pas, LeixBot ne prend pas de pause MrDestructoid",
         )
 
+    @commands.Component.listener()
     async def event_bits_used(self, message: twitchio.ChannelBitsUse) -> None:
         LOGGER.info(f"{message.user.name} used {message.bits} bits on {message.broadcaster.display_name}!")
         await message.broadcaster.send_message(
@@ -72,6 +80,7 @@ class Subscriptions(commands.Component):
             message=f"Merci {message.user.display_name} pour les {message.bits} bits! Tu connais TwitchRPG? 👀",
         )
 
+    @commands.Component.listener()
     async def event_hype_train_begin(self, message: twitchio.HypeTrainBegin) -> None:
         LOGGER.info(f"Hype Train started on {message.broadcaster.name}!")
         await message.broadcaster.send_message(
